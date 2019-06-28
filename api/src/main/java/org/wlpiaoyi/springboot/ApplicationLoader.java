da6ba80b23c917dc6b10c86c0397918a12fad1fb
wlpiaoyi <qqpiaoyi@126.com>
2019-06-28 14:00:41 +0800

    test
    test

--- api/src/main/java/org/wlpiaoyi/springboot/ApplicationLoader.java
+++ api/src/main/java/org/wlpiaoyi/springboot/ApplicationLoader.java
@@ -19,24 +19,11 @@ public class ApplicationLoader implements ApplicationContextAware {
         SpringApplication.run(ApplicationLoader.class, args);
     }
 
-
     public static <T> T getBean(Class<T> tClass) {
         return APPLICATION_CONTEXT.getBean(tClass);
     }
 
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         APPLICATION_CONTEXT = applicationContext;
-        new Thread(new Runnable() {
-            public void run() {
-                while (true){
-                    try {
-                        Thread.sleep(500);
-
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }).start();
     }
 }
